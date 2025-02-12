140f713abe46b567831612994c74c81182ef3969
“Aleksey Ippolitov” <setgecko@gmail.com>
2025-02-12 12:23:58 +0300

    Выполнил задачи по чтению данных из json файлов

--- ReadFromJSON.py
+++ ReadFromJSON.py
@@ -34,7 +34,22 @@ print(data["is_student"]) # Вывод: False
 Прочитай файл user.json и создай объект класса User.
 Вызови метод get_info().
 '''
+import json
+class User:
+    def __init__(self, name, age, is_student, skills):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+        self.skills = skills
+    def get_info(self):
+        return f"Имя: {self.name}, Возраст: {self.age}, Студент: {self.is_student}, Навыки: {self.skills}"
 
+
+with open("user.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+me = User(data["name"],data["age"], data["is_student"], data["skills"])
+print(me.get_info())
 '''
 Задача 2: Список книг
 Описание: У тебя есть JSON-файл books.json со списком книг
@@ -43,13 +58,51 @@ print(data["is_student"]) # Вывод: False
 Создай базовый класс Book, который хранит название (title) и автора (author).
 Создай дочерний класс Ebook, унаследованный от Book. Добавь атрибут format.
 Создай дочерний класс PaperBook, унаследованный от Book. Добавь атрибут pages.
+
 Определи метод get_info() для каждого класса, чтобы он возвращал разные строки:
 Для Ebook: "Электронная книга: [title], Автор: [author], Формат: [format]".
 Для PaperBook: "Печатная книга: [title], Автор: [author], Страниц: [pages]".
 Прочитай файл books.json и создай список объектов (Ebook или PaperBook в зависимости от данных).
 Вызови метод get_info() для каждого объекта.
 '''
+import  json
 
+with open("books.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_info(self):
+        return "Информация о книге"
+
+class Ebook(Book):
+    def __init__(self, title, author, format):
+        super().__init__(author, title)
+        self.format = format
+    def get_info(self):
+        return f"Электронная книга: {self.title}, Автор: {self.author}, Формат: {self.format}"
+
+class PaperBook(Book):
+    def __init__(self, title, author, pages):
+        super().__init__(title, author)
+        self.pages = pages
+    def get_info(self):
+        return f"Электронная книга: {self.title}, Автор: {self.author}, Формат: {self.pages}"
+
+books_list = []
+
+for book_data in data:
+    if "format" in book_data:
+        eb = Ebook(book_data["title"], book_data["author"], book_data["format"])
+        books_list.append(eb)
+    elif "pages" in book_data:
+        pb = PaperBook(book_data["title"], book_data["author"], book_data["pages"])
+        books_list.append(pb)
+
+for book in books_list:
+    print(book.get_info())
 '''
 Задача 3: Автопарк
 Описание: У тебя есть JSON-файл cars.json с данными об автомобилях
@@ -64,7 +117,43 @@ print(data["is_student"]) # Вывод: False
 Прочитай файл cars.json и создай список объектов (Sedan или Truck в зависимости от типа).
 Вызови метод get_info() для каждого объекта.
 '''
+import json
 
+with open("cars.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+    def get_info(self):
+        return "Информация об автомобиле"
+
+class Sedan(Car):
+    def __init__(self,  brand, year):
+        super().__init__(brand, year)
+    def get_info(self):
+        return f"Легковой автомобиль: {self.brand}, Год: {self.year}"
+
+class Truck(Car):
+    def __init__(self,  brand, year, load_capacity):
+        super().__init__(brand, year)
+        self.load_capacity = load_capacity
+    def get_info(self):
+        return f"Грузовик: {self.brand}, Год: {self.year}, Грузоподъемность: {self.load_capacity}"
+
+cars = []
+
+for car_data in data:
+    if "load_capacity" in car_data:
+        eb = Truck(car_data["brand"], car_data["year"], car_data["load_capacity"])
+        cars.append(eb)
+    else:
+        pb = Sedan(car_data["brand"], car_data["year"])
+        cars.append(pb)
+
+for car in cars:
+    print(car.get_info())
 '''
 Задача 4: Города
 Описание: У тебя есть JSON-файл cities.json с данными о городах
@@ -79,7 +168,47 @@ print(data["is_student"]) # Вывод: False
 Прочитай файл cities.json и создай список объектов (Capital или SmallCity в зависимости от значения is_capital).
 Вызови метод get_info() для каждого объекта.
 '''
+import json
 
+with open("cities.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def get_info(self):
+        return "Информация о городе"
+
+class Capital(City):
+    def __init__(self, name, population, country):
+        super().__init__(name, population)
+        self.country = country
+
+    def get_info(self):
+        return f"Столица: {self.name}, Население: {self.population}, Страна: {self.country}"
+
+class SmallCity(City):
+    def __init__(self, name, population):
+        super().__init__(name, population)
+
+    def get_info(self):
+        return f"Город: {self.name}, Население: {self.population}"
+
+cities = []
+
+for c in data:
+    if c.get("is_capital", False):
+        capital = Capital(c["name"], c["population"], c["country"])
+        cities.append(capital)
+    else:
+        small_city = SmallCity(c["name"], c["population"])
+        cities.append(small_city)
+
+
+for city in cities:
+    print(city.get_info())
 '''
 Задача 5: Банковские счета
 Описание: У тебя есть JSON-файл accounts.json с данными о счетах
@@ -94,4 +223,59 @@ print(data["is_student"]) # Вывод: False
 Прочитай файл accounts.json и создай список объектов (SavingsAccount или CreditCard в зависимости от типа).
 Вызови метод withdraw(amount) для каждого объекта.
 '''
+import json
 
+# Чтение JSON-файла
+with open("accounts.json", "r", encoding="utf-8") as file:
+    data = json.load(file)  # data — это список словарей
+
+
+# Базовый класс BankAccount
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+
+# Дочерний класс SavingsAccount
+class SavingsAccount(BankAccount):
+    def __init__(self, balance, interest_rate):
+        super().__init__(balance)
+        self.interest_rate = interest_rate
+
+    def withdraw(self, amount):
+        lim = (self.balance / 100) * 90  # Максимум 90% баланса
+        if amount <= lim:  # Проверяем лимит
+            self.balance -= amount
+            return f"Снято {amount}. Новый баланс: {self.balance}"
+        else:
+            return "Нельзя снять больше 90% баланса"
+
+
+# Дочерний класс CreditCard
+class CreditCard(BankAccount):
+    def __init__(self, balance, limit):
+        super().__init__(balance)
+        self.limit = limit
+
+    def withdraw(self, amount):
+        if amount > self.limit or amount > self.balance:  # Проверяем лимит и баланс
+            return "Операция невозможна"
+        else:
+            self.balance -= amount
+            return f"Снято {amount}. Новый баланс: {self.balance}"
+
+
+# Создаем список объектов
+bank_accounts = []
+
+for b in data:  # Проходим по каждому элементу списка
+    if b.get("type") == "savings":  # Проверяем тип счета
+        sa = SavingsAccount(b["balance"], b["interest_rate"])
+        bank_accounts.append(sa)
+    elif b.get("type") == "credit_card":  # Проверяем тип счета
+        cc = CreditCard(b["balance"], b["limit"])
+        bank_accounts.append(cc)
+
+# Вызываем метод withdraw() для каждого объекта
+for account in bank_accounts:
+    print(account.withdraw(1000))
