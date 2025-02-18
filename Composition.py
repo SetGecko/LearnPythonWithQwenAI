b20c5f0ca4d5f3fbb4124028215da796ce02ce97
“Aleksey Ippolitov” <setgecko@gmail.com>
2025-02-18 23:15:47 +0300

    Сделал задачи 4-10 по принципу ООП Композиция

--- Composition.py
+++ Composition.py
@@ -89,25 +89,25 @@ print(car.get_info())
 Вызывает метод start() у объекта self.engine.
 Возвращает строку "Автомобиль: [brand], Двигатель: [power] л.с.".
 '''
-# class Engine:
-#     def __init__(self, power):
-#         self.power = power
-#
-#     def start(self):
-#         return f"Двигатель мощностью {self.power} л.с. запущен"
-#
-# class Car:
-#     def __init__(self, brand, power):
-#         self.brand = brand
-#         self.engine = Engine(power)
-#     def get_info(self):
-#         return (
-#             f"Автомобиль: {self.brand}\n"
-#             f"{self.engine.start()}\n"
-#         )
-#
-# car = Car("Daewoo Nexia", 87)
-# print(car.get_info())
+class Engine:
+    def __init__(self, power):
+        self.power = power
+
+    def start(self):
+        return f"Двигатель мощностью {self.power} л.с. запущен"
+
+class Car:
+    def __init__(self, brand, power):
+        self.brand = brand
+        self.engine = Engine(power)
+    def get_info(self):
+        return (
+            f"Автомобиль: {self.brand}\n"
+            f"{self.engine.start()}\n"
+        )
+
+car = Car("Daewoo Nexia", 87)
+print(car.get_info())
 '''
 Задача 2: Компьютер
 Алгоритм:
@@ -182,7 +182,25 @@ print(pers.get_address())
 Вызывает метод get_name() у объекта self.director.
 Возвращает строку "Фильм: [title], Режиссер: [name]".
 '''
+class Director:
+    def __init__(self, name):
+        self.name = name
 
+    def get_name(self):
+        return f"Режиссер: {self.name}"
+
+class Film:
+    def __init__(self, title, name):
+        self.title = title
+        self.name = Director(name)
+
+    def get_info(self):
+        return (
+            f"Фильм: {self.title}, {self.name.get_name()}"
+        )
+
+ff = Film("Чужой", "Риддли Скотт")
+print(ff.get_info())
 '''
 Задача 5: Книга
 Алгоритм:
@@ -194,7 +212,25 @@ print(pers.get_address())
 Вызывает метод get_name() у объекта self.author.
 Возвращает строку "Книга: [title], Автор: [name]".
 '''
+class Author:
+    def __init__(self, name):
+        self.name = name
 
+    def get_name(self):
+        return f"Автор: {self.name}"
+
+class Book:
+    def __init__(self, title, name):
+        self.title = title
+        self.name = Author(name)
+
+    def get_info(self):
+        return (
+            f"Книга: {self.title}, {self.name.get_name()}"
+        )
+
+bb = Book("Пикник у обочины", "Братья Стругацкие")
+print(bb.get_info())
 '''
 Задача 6: Город
 Алгоритм:
@@ -206,7 +242,26 @@ print(pers.get_address())
 Вызывает метод get_location() у объекта self.location.
 Возвращает строку "Город: [name], Координаты: [latitude], [longitude]".
 '''
+class Location:
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
 
+    def get_location(self):
+        return f"Координаты: {self.latitude}, {self.longitude}"
+
+class City:
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = Location(latitude, longitude)
+
+    def get_info(self):
+        return (
+            f"Город: {self.name}, {self.latitude.get_location()}"
+        )
+
+cc = City("Краснодар", 400, 599)
+print(cc.get_info())
 '''
 Задача 7: Банковский счет
 Алгоритм:
@@ -220,7 +275,34 @@ print(pers.get_address())
 Возвращает результат операции.
 Определи метод get_balance_info(), который вызывает метод get_balance() у объекта self.balance.
 '''
+class Balance:
+    def __init__(self, balance):
+        self.balance = balance
 
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            return f"Текущй баланс = {self.balance}"
+        else:
+            return "Недостаточно средств"
+
+    def get_balance(self):
+        return f"Текущий баланс = {self.balance}"
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = Balance(balance)
+
+    def withdraw_money(self, amount):
+        return f"{self.balance.withdraw(amount)}"
+
+    def get_balance_info(self):
+        return f"{self.balance.get_balance()}"
+
+zz = BankAccount("Алексей", 9000)
+print(zz.get_balance_info())
+print(zz.withdraw_money(3000))
 '''
 Задача 8: Телефон
 Алгоритм:
@@ -234,7 +316,37 @@ print(pers.get_address())
 Выводит сообщение "Звонок продолжительностью [duration] минут".
 Определи метод check_battery(), который вызывает метод get_charge() у объекта self.battery.
 '''
+class Battery:
+    def __init__(self, charge):
+        self.charge = charge
 
+    def use_battery(self, usage):
+        if self.charge >= usage:
+            self.charge -= usage
+            return True
+        else:
+            return False
+
+    def get_charge(self):
+        return f"Текущий заряд = {self.charge}"
+
+class Phone:
+    def __init__(self, number, charge):
+        self.number = number
+        self.charge = Battery(charge)
+
+    def check_battery(self):
+        return f"{self.charge.get_charge()}"
+
+    def make_call(self, duration):
+        if self.charge.use_battery(duration) == True:
+            return f"Звонок продолжительностью {duration} минут"
+        else:
+            return "Нельзя!"
+
+zz = Phone(8800, 100)
+print(zz.check_battery())
+print(zz.make_call(200))
 '''
 Задача 9: Машина времени
 Алгоритм:
@@ -246,17 +358,77 @@ print(pers.get_address())
 Определи метод travel_to_year(), который вызывает метод set_year() у объекта self.control.
 Определи метод get_travel_info(), который вызывает метод get_year() у объекта self.control.
 '''
+class TimeMachineControl:
+    def __init__(self, year):
+        self.year = year
 
+    def set_year(self, new_year):
+        if self.year <= 0:
+            return False
+        else:
+            self.year = new_year
+            return self.year
+
+    def get_year(self):
+        return self.year
+
+class TimeMachine:
+    def __init__(self, name, year):
+        self.name = name
+        self.control = TimeMachineControl(year)
+
+    def travel_to_year(self, year):
+        return f"{self.control.set_year(year)}"
+
+    def get_travel_info(self):
+        return f"{self.control.get_year()}"
+
+mm = TimeMachine("Алексей", 1900)
+print(mm.travel_to_year(1700))
+print(mm.get_travel_info())
 '''
 Задача 10: Магазин
 Алгоритм:
 Создай класс Product, который хранит название товара (name) и цену (price).
 Определи метод get_info(), который возвращает строку "Товар: [name], Цена: [price]".
+
 Создай класс Store, который хранит название магазина (name) и список товаров (products).
 В методе __init__ создай пустой список товаров (self.products = []).
 Определи метод add_product(name, price), который:
 Создает объект класса Product и добавляет его в список self.products.
+
 Определи метод get_products_info(), который:
 Перебирает все объекты в списке self.products.
 Вызывает метод get_info() у каждого объекта и выводит результат.
 '''
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def get_info(self):
+        return f"Товар: {self.name}, Цена: {self.price}"
+
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, name, price):
+        product = Product(name, price)
+        self.products.append(product)
+
+    def get_products_info(self):
+        if not self.products:
+            print("В магазине нет товаров.")
+            return
+
+        for product in self.products:
+            print(product.get_info())
+
+store = Store("Магнит")
+
+store.add_product("Хлеб", 50)
+store.add_product("Молоко", 80)
+store.add_product("Яблоки", 60)
+store.get_products_info()
